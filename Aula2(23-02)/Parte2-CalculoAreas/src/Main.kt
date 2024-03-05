c28765deaa0c049d1110bf17eeeaa349c619981d
Igor <igor.pessoa@etec.sp.gov.br>
2024-03-05 10:23:39 -0300

    PAM2-23-02-24

--- Aula2(23-02)/Parte2-CalculoAreas/src/Main.kt
+++ Aula2(23-02)/Parte2-CalculoAreas/src/Main.kt
@@ -18,63 +18,49 @@ fun main(args: Array<String>)
     }
 }
 fun areaRetang() {
-    println("Digite a base do retangulo:")
-    val base = readLine()?.toDoubleOrNull() ?: 0.0
-    println("Digite a altura do retangulo:")
-    val altura = readLine()?.toDoubleOrNull() ?: 0.0
-
+    val base = 10
+    val altura = 2
     val area = (base * altura) / 2
-    println("A área do triângulo é: $area")
+    println("Valor da base: $base, valor da altura: $altura. A área do triângulo é: $area")
 }
 
 fun areaEqui() {
-    println("Digite o lado do triangulo:")
-    val lado = readLine()?.toDoubleOrNull() ?: 0.0
+    val lado = 10
     val area = (sqrt(3.0) / 4) * lado * lado
-    println("A área do triângulo equilatero é: $area")
+    println("O lado é: $lado, e a área do triângulo equilatero é: $area")
 }
 
 fun areaEsca(){
-    println("Digite o comprimento do triangulo:")
-    val cump = readLine()?.toDoubleOrNull() ?: 0.0
-    println("Digite o a altura do triangulo:")
-    val alt = readLine()?.toDoubleOrNull() ?: 0.0
+    val cump = 10
+    val alt = 2
     val area = (cump*alt)/2
-    println("A área do triângulo escaleno é: $area")
+    println("O cumprimento é: $cump, e a altura: $alt. A área do triângulo escaleno é: $area")
 }
 
 fun areaIso(){
-    println("Digite o comprimento do triangulo:")
-    val cump = readLine()?.toDoubleOrNull() ?: 0.0
-    println("Digite o a altura do triangulo:")
-    val alt = readLine()?.toDoubleOrNull() ?: 0.0
+    val cump = 10
+    val alt = 2
     val area = (cump*alt)/2
-    println("A área do triângulo isósceles é: $area")
+    println("O cumprimento é: $cump, e a altura: $alt. A área do triângulo isósceles é: $area")
 }
 
 fun areaCircun(){
-    println("Digite o raio da circunferencia:")
-    val raio = readLine()?.toDoubleOrNull() ?: 0.0
+    val raio = 10
     val area = PI * (raio*raio)
-    println("A área da circunferência é: $area")
+    println("O raio é: $raio. A área da circunferência é: $area")
 }
 
 fun areaLosan(){
-    println("Digite o valor da diagonal maior:")
-    val DMaior = readLine()?.toDoubleOrNull() ?: 0.0
-    println("Digite o valor da diagonal menor:")
-    val DMenor = readLine()?.toDoubleOrNull() ?: 0.0
+    val DMaior = 10
+    val DMenor = 2
     val area = (DMaior*DMenor) / 2
-    println("A área da circunferência é: $area")
+    println("A diagonal maior é: $DMaior, e a menor é: $DMenor. A área do losango é: $area")
 }
 
 fun areaTrap(){
-    println("Digite o valor da base maior:")
-    val BMaior = readLine()?.toDoubleOrNull() ?: 0.0
-    println("Digite o valor da base menor:")
-    val BMenor = readLine()?.toDoubleOrNull() ?: 0.0
-    println("Digite o valor da altura:")
-    val alt = readLine()?.toDoubleOrNull() ?: 0.0
+    val BMaior = 10
+    val BMenor = 2
+    val alt = 10
     val area = ((BMaior + BMenor) * alt) / 2
-    println("A área da circunferência é: $area")
+    println("A base maior é: $BMaior, a menor é: $BMenor, e a altura é: $alt. A área é: $area")
 }
